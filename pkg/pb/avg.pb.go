041199c3159a62b383c2acc58fe9d026a9a6a243
Matt Bell <matt@mbell.me>
2019-04-13 17:44:18 +0100

    Update codegen

--- pkg/pb/avg.pb.go
+++ pkg/pb/avg.pb.go
@@ -190,7 +190,7 @@ func init() {
 func init() { proto.RegisterFile("avg.proto", fileDescriptor_b889feec73713d05) }
 
 var fileDescriptor_b889feec73713d05 = []byte{
-	// 169 bytes of a gzipped FileDescriptorProto
+	// 185 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0xe2, 0x4c, 0x2c, 0x4b, 0xd7,
 	0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0x62, 0x4e, 0x2c, 0x4b, 0x57, 0x52, 0xe3, 0x12, 0xf0, 0xcc,
 	0x2b, 0x71, 0x2c, 0x2a, 0x4a, 0xac, 0x74, 0x2f, 0x4a, 0x4d, 0x2d, 0xc9, 0xcc, 0x4b, 0x17, 0x12,
@@ -198,10 +198,11 @@ var fileDescriptor_b889feec73713d05 = []byte{
 	0x94, 0xb8, 0x38, 0x3c, 0xf3, 0x4a, 0x82, 0x52, 0x0b, 0x72, 0x2a, 0x85, 0xc4, 0xb8, 0xd8, 0x8a,
 	0x52, 0x8b, 0x4b, 0x73, 0x4a, 0x24, 0x18, 0x15, 0x18, 0x35, 0x58, 0x83, 0xa0, 0x3c, 0x25, 0x0d,
 	0x2e, 0x21, 0xb7, 0x9c, 0xfc, 0x44, 0x3c, 0xa6, 0x31, 0x41, 0x4d, 0x53, 0xe1, 0xe2, 0x02, 0xab,
-	0xc4, 0x66, 0x1e, 0x13, 0xcc, 0x3c, 0x23, 0x6b, 0x2e, 0x76, 0xb0, 0x29, 0xa9, 0x45, 0x42, 0x06,
+	0xc4, 0x66, 0x1e, 0x13, 0xcc, 0x3c, 0xa3, 0x1c, 0x2e, 0x76, 0xb0, 0x29, 0xa9, 0x45, 0x42, 0x06,
 	0x5c, 0x2c, 0xbe, 0xa9, 0x89, 0x79, 0x42, 0xa2, 0x7a, 0x20, 0xf7, 0xa3, 0xbb, 0x58, 0x8a, 0x1f,
-	0x2c, 0x8c, 0x30, 0x52, 0x89, 0x21, 0x89, 0x0d, 0xec, 0x49, 0x63, 0x40, 0x00, 0x00, 0x00, 0xff,
-	0xff, 0x47, 0x9d, 0xbf, 0x82, 0xf1, 0x00, 0x00, 0x00,
+	0x2c, 0x8c, 0x30, 0x52, 0x89, 0x41, 0x48, 0x9f, 0x8b, 0x35, 0x28, 0x31, 0x2f, 0x3d, 0x15, 0x97,
+	0x16, 0x5e, 0x98, 0x30, 0x54, 0x43, 0x12, 0x1b, 0x38, 0x54, 0x8c, 0x01, 0x01, 0x00, 0x00, 0xff,
+	0xff, 0x38, 0xd7, 0x03, 0x56, 0x22, 0x01, 0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -218,6 +219,7 @@ const _ = grpc.SupportPackageIsVersion4
 type GreeterClient interface {
 	// Calculates mean average
 	Mean(ctx context.Context, in *IntArrayGreeting, opts ...grpc.CallOption) (*FloatReply, error)
+	Range(ctx context.Context, in *IntArrayGreeting, opts ...grpc.CallOption) (*IntReply, error)
 }
 
 type greeterClient struct {
@@ -237,10 +239,20 @@ func (c *greeterClient) Mean(ctx context.Context, in *IntArrayGreeting, opts ...
 	return out, nil
 }
 
+func (c *greeterClient) Range(ctx context.Context, in *IntArrayGreeting, opts ...grpc.CallOption) (*IntReply, error) {
+	out := new(IntReply)
+	err := c.cc.Invoke(ctx, "/avg.Greeter/Range", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // GreeterServer is the server API for Greeter service.
 type GreeterServer interface {
 	// Calculates mean average
 	Mean(context.Context, *IntArrayGreeting) (*FloatReply, error)
+	Range(context.Context, *IntArrayGreeting) (*IntReply, error)
 }
 
 // UnimplementedGreeterServer can be embedded to have forward compatible implementations.
@@ -249,6 +261,9 @@ type UnimplementedGreeterServer struct {
 
 func (*UnimplementedGreeterServer) Mean(ctx context.Context, req *IntArrayGreeting) (*FloatReply, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method Mean not implemented")
+}
+func (*UnimplementedGreeterServer) Range(ctx context.Context, req *IntArrayGreeting) (*IntReply, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method Range not implemented")
 }
 
 func RegisterGreeterServer(s *grpc.Server, srv GreeterServer) {
@@ -273,6 +288,24 @@ func _Greeter_Mean_Handler(srv interface{}, ctx context.Context, dec func(interf
 	return interceptor(ctx, in, info, handler)
 }
 
+func _Greeter_Range_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(IntArrayGreeting)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(GreeterServer).Range(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/avg.Greeter/Range",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(GreeterServer).Range(ctx, req.(*IntArrayGreeting))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 var _Greeter_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "avg.Greeter",
 	HandlerType: (*GreeterServer)(nil),
@@ -280,6 +313,10 @@ var _Greeter_serviceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "Mean",
 			Handler:    _Greeter_Mean_Handler,
+		},
+		{
+			MethodName: "Range",
+			Handler:    _Greeter_Range_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
