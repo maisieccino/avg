7f10014c80bce74ac51ce7fdac9a662633fa22d1
Matt Bell <matt@mbell.me>
2019-04-14 21:25:32 +0100

    Add mean tests

--- pkg/svc/svc.go
+++ pkg/svc/svc.go
@@ -28,5 +28,13 @@ func NewBasicService() Service {
 
 func (svc basicService) Mean(_ context.Context, nums []int32) (float32, error) {
 	// TODO: implement
-	return 0.0, nil
+	var sum, length int32
+	for _, num := range nums {
+		length = length + 1
+		sum = sum + num
+	}
+	if length == 0 {
+		return 0.0, nil
+	}
+	return float32(sum) / float32(length), nil
 }
