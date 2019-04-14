9386c05d5869e3240e786a22cecb1cc760a1221b
Matt Bell <matt@mbell.me>
2019-04-14 11:53:42 +0100

    Fix client builder script

--- ci/scripts/build-client.sh
+++ ci/scripts/build-client.sh
@@ -1,10 +1,10 @@
 #!/bin/sh -xe
+apk add --no-cache ca-certificates git protobuf bash
+go get -u github.com/golang/protobuf/protoc-gen-go           
 export GO111MODULE=on
 export GOOS="$os"
 export GOARCH="$arch"
 cd src
-apk add --no-cache ca-certificates git protobuf bash
-GO111MODULE=off CGO_ENABLED=0 go get -u github.com/golang/protobuf/protoc-gen-go           
 go mod download
 ./hack/verify_codegen.sh
 version=$(cat ../$version_file)
